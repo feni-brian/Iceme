3de024b700bb2bedb600c9ea89b747d97fef7639
feni-brian <feni.brian@icloud.com>
2023-07-14 15:34:02 +0300

    Added colours and app icon to the general app assets folder.

---
+++ Iceme Corner/App.swift
@@ -0,0 +1,17 @@
+//
+//  Iceme_CornerApp.swift
+//  Iceme Corner
+//
+//  Created by Feni Brian on 14/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Iceme_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
