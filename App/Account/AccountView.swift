76780562b6c19bd10ab599f1d4de86b11251d20e
feni-brian <56452538+feni-brian@users.noreply.github.com>
2023-07-30 17:48:00 +0300

    Added user account and sign up views.

--- App/Account/AccountView.swift
+++ App/Account/AccountView.swift
@@ -6,15 +6,97 @@
 //
 
 import SwiftUI
+import StoreKit
+import FoodTruckKit
 
 struct AccountView: View {
+    @EnvironmentObject private var accountStore: AccountStore
+    @ObservedObject var model: FoodTruckModel
+    @State private var isSignUpSheetPresented: Bool = false
+    @State private var isSignOutAlertPresented: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            if case let .authenticated(username) = accountStore.currentUser {
+                Section {
+                    HStack {
+                        Image(systemName: "person.fill")
+                            .font(.system(.largeTitle, design: .rounded))
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .frame(width: 60, height: 60)
+                            .background(Color.accentColor.gradient, in: Circle())
+                        Text(username)
+                            .font(.system(.title3, design: .rounded))
+                            .fontWeight(.medium)
+                    }
+                }
+            }
+            #if os(iOS)
+            NavigationLink(value: "In-app purhcase support") {
+                Label("In-app purchase support", systemImage: "questionmark.circle")
+            }
+            #else
+            Section {
+                Button("Restore missing purchase") {
+                    Task(priority: .userInitiated, operation: { try await AppStore.sync() })
+                }
+                .frame(maxWidth: .infinity)
+            }
+            #endif
+            
+            Section {
+                if accountStore.isSignedIn {
+                    Button("Sign Out", role: .destructive, action: { isSignOutAlertPresented = true })
+                        .frame(maxWidth: .infinity)
+                } else {
+                    Button("Sign In", action: { signIn() })
+                    Button("Sign Up", action: { isSignUpSheetPresented = true })
+                }
+            }
+        }
+        .navigationTitle("Account")
+        #if os(iOS)
+        .navigationDestination(for: String.self) { _ in
+            StoreSupportView()
+        }
+        #endif
+        .sheet(isPresented: $isSignUpSheetPresented) {
+            NavigationStack {
+                SignUpView(model: model)
+            }
+        }
+        .alert(isPresented: $isSignOutAlertPresented, content: { signOutAlert })
+    }
+    
+    private var signOutAlert: Alert {
+        Alert(
+            title: Text("Are you sure you want to sign out?"),
+            primaryButton: .destructive(Text("Sign Out")) { accountStore.signOut() },
+            secondaryButton: .cancel()
+        )
+    }
+    
+    private func signIn() {
+        Task { @MainActor in
+            try await accountStore.signIn()
+        }
     }
 }
 
 struct AccountView_Previews: PreviewProvider {
+    struct Preview: View {
+        @StateObject private var model = FoodTruckModel()
+        @StateObject private var accountStore = AccountStore()
+        var body: some View {
+            AccountView(model: model)
+                .environmentObject(accountStore)
+        }
+    }
+    
     static var previews: some View {
-        AccountView()
+        NavigationStack {
+            Preview()
+        }
     }
 }
--- App/Account/SignUpView.swift
+++ App/Account/SignUpView.swift
@@ -6,15 +6,118 @@
 //
 
 import SwiftUI
+import FoodTruckKit
 
 struct SignUpView: View {
+    @EnvironmentObject private var accountStore: AccountStore
+    @FocusState private var focusedElement: FocusElement?
+    @Environment(\.dismiss) private var dismiss
+    @ObservedObject var model: FoodTruckModel
+    @State private var signUpType: SignUpType = .passkey
+    @State private var username = ""
+    @State private var password = ""
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section {
+                TextField("User's Name", text: $username)
+                    .textContentType(.username)
+                #if os(iOS)
+                    .textInputAutocapitalization(.never)
+                    .keyboardType(.emailAddress)
+                #endif
+                    .focused($focusedElement, equals: .username)
+                if case .password = signUpType {
+                    SecureField("Password", text: $password)
+                        .textContentType(.password)
+                        .focused($focusedElement, equals: .password)
+                }
+            } footer: {
+                if case .passkey = signUpType {
+                    HStack(spacing: 10) {
+                        Image(systemName: "person.badge.key.fill")
+                            .font(.title2)
+                        Text("When you sign up with a passkey, all you need is a user name. The passkey will be available on all of your devices.")
+                    }
+                }
+            }
+            Section {
+                Button("Sign Up", action: { signUp() })
+                    .frame(maxWidth: .infinity)
+                    .font(.headline)
+                    .disabled(!isFormValid)
+            } footer: {
+                changeSignUpTypeButton
+            }
+        }
+        .navigationTitle("Sign Up")
+        #if os(macOS)
+        .defaultFocus($focusedElement, .username)
+        #endif
+        .toolbar {
+            Button("Cancel", role: .cancel, action: { dismiss() })
+        }
+    }
+    
+    private enum FocusElement {
+        case username
+        case password
+    }
+    
+    private enum SignUpType {
+        case passkey
+        case password
+    }
+    
+    private var isFormValid: Bool {
+        switch signUpType {
+        case .passkey: return !username.isEmpty
+        case .password: return !username.isEmpty && !password.isEmpty
+        }
+    }
+    
+    @ViewBuilder
+    private var changeSignUpTypeButton: some View {
+        Group {
+            switch signUpType {
+            case .passkey: Button("Sign up with password", action: { signUpType = .password })
+            case .password:
+                Button("Sign up with passkey") {
+                    signUpType = .passkey
+                    password = ""
+                }
+            }
+        }
+        .padding(8)
+        .frame(maxWidth: .infinity)
+        .font(.system(.subheadline))
+    }
+    
+    private func signUp() {
+        Task { @MainActor in
+            switch signUpType {
+            case .passkey: try await accountStore.createPasskeyAccount(username: username)
+            case .password: try await accountStore.createPasswordAccount(username: username, password: password)
+            }
+            dismiss()
+        }
     }
 }
 
 struct SignUpView_Previews: PreviewProvider {
+    struct Preview: View {
+        @StateObject private var accountStore = AccountStore()
+        @StateObject private var model = FoodTruckModel()
+        var body: some View {
+            SignUpView(model: model)
+                .environmentObject(accountStore)
+        }
+    }
+    
     static var previews: some View {
-        SignUpView()
+        NavigationStack {
+            Preview()
+        }
     }
 }
